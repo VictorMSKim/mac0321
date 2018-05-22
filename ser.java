feb4fe06626de8bf0badcf5250b77f892da16614
VictorMSKim <victorminsubkim@gmail.com>
2018-05-22 03:41:18 -0300

    adicionado classe ser.java

---
+++ ser.java
@@ -0,0 +1,35 @@
+public Ser {
+	private String nome;
+	private String genero;
+	private String especie;
+	private String dataDeNascimento;
+	private String localDeOrigem;
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	public String getEspecie() {
+		return especie;
+	}
+	public void setDataDeNascimento(String dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+	public String getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+	public void setLocalDeOrigem(String localDeOrigem) {
+		this.localDeOrigem = localDeOrigem;
+	}
+}
+
